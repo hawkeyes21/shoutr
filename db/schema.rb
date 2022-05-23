46acba37e3dcfc8bbca62b95af2305be796826b5
Jaymin <jaymin@pop-os.localdomain>
2022-05-23 15:09:08 +0530

    adds new field username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_091251) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_092427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,8 +21,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_091251) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
