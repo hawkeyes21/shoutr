b03aae57f4b7fbcfe7a166680017d467c8a5ccae
Jaymin Shah <shahjaymin2101@gmail.com>
2022-06-01 15:24:16 +0530

    creates avatar_helper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,10 +1,5 @@
 module ShoutHelper
-    def avatar(user)
-        email_digest = Digest::MD5.hexdigest(user.email)
-        gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-        image_tag gravatar_url
-    end
-
+    
     def like_button(shout)
         if current_user.liked?(shout)
             button_to "Unlike", unlike_shout_path(shout), method: :delete 
@@ -16,4 +11,5 @@ module ShoutHelper
     def autolink(text)
         text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
     end
+    
 end
